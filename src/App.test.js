7248512412ac176d9b37fe5dd3410408cc8ce593
Anil Pai <apai@vrbo.com>
2019-10-14 15:14:27 -0500

    Added Flow

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import App from "./App";
+import React from 'react'
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import App from './App'
 
-it("renders welcome message", () => {
-  const { getByText } = render(<App />);
-  expect(getByText("Learn React")).toBeInTheDocument();
-});
+it('renders welcome message', () => {
+  const { getByText } = render(<App />)
+  expect(getByText('Learn React')).toBeInTheDocument()
+})

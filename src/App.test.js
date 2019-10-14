b05b859a90e4ba5c4e83ab07155ed3d608c5144b
Anil Pai <apai@vrbo.com>
2019-10-14 12:13:35 -0500

    :tada: Added Testing library

---
+++ src/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {render} from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
+import App from './App';
+
+it('renders welcome message', () => {
+  const { getByText } = render(<App />);
+  expect(getByText('Learn React')).toBeInTheDocument();
+
+});
